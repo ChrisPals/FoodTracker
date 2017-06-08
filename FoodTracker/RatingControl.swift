2e59d9fe177ff0a6a3b10c22dceef1f7170536fa
Christoffer Palsgaard <CP@cp-mac.local>
2017-06-08 16:24:41 +0200

    second commit

---
+++ FoodTracker/RatingControl.swift
@@ -0,0 +1,21 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by Christoffer Palsgaard on 08/06/2017.
+//  Copyright © 2017 Christoffer Palsgaard. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
